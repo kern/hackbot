bf37a7b7ed97a2ed99e83793744c47558515ab91
Alex Kern <alex@kern.io>
2015-01-21 15:00:13 -0800

    A bit more express.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var config = require('./config.json');
+var express = require('express');
 
 var FB = require('fb');
 FB.setAccessToken(process.env.ACCESS_TOKEN);
@@ -6,6 +7,8 @@ FB.setAccessToken(process.env.ACCESS_TOKEN);
 var refreshLocked = require('./locked').refresh;
 
 function refresh() {
+  console.log('Refreshing...');
+
   refreshLocked(config.groupID, config.adminIDs).then(function(cids) {
     cids.forEach(function(cid) {
       console.log('Deleted: ' + cid);
@@ -18,3 +21,11 @@ function refresh() {
 }
 
 refresh();
+
+var app = express();
+
+app.get('/', function(req, res) {
+  res.send("Hello from Hackathon Hackbot! <3");
+});
+
+app.listen(process.env.PORT || 3000);
